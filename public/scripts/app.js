54796fec124e14a9e0bddefb56e6a7f411b84d74
Brady Withers <brady.withers@outlook.com>
2019-01-30 16:40:33 -0700

    created package.json with babel and yarn

---
+++ public/scripts/app.js
@@ -0,0 +1,15 @@
+console.log('app.js is running!');
+
+//JSX - Javascript XML
+// var template = <p>This is JSX from app.js</p>;
+
+var template = React.createElement(
+    "p",
+    null,
+    "This is JSX from app.js"
+  );
+  
+var appRoot = document.getElementById('app');
+
+
+ReactDOM.render(template, appRoot);
