99318ebe51e8911134d562bbfdb3b7c288ec8e38
Brady Withers <brady.withers@outlook.com>
2019-02-08 14:04:36 -0700

    sub classes

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -12,8 +12,44 @@ class Person {
     }
 }
 
-const me = new Person('Brady Withers', 39);
-console.log(me.getDescription());
+
+class Student extends Person {
+    constructor(name, age, major) {
+        super(name, age);
+        this.major = major;
+    }
+    hasMajor() {
+        return !!this.major;
+    }
+    getDescription(){
+        let description = super.getDescription();
+
+        if(this.hasMajor()) {
+            description += ` their major is ${this.major}.`;
+        }
+
+        return description;
+    }
+}
+
+class Traveler extends Person {
+    constructor(name, age, homeLocation) {
+        super(name, age);
+        this.homeLocation = homeLocation;
+    }
+    getGreeting() {
+        let greeting = super.getGreeting();
+
+        if (this.homeLocation) {
+            greeting += ` I am from ${this.homeLocation}`;
+        }
+
+        return greeting;
+    }
+}
+
+const me = new Traveler ('Brady Withers', 39, 'Herriman');
+console.log(me.getGreeting());
 
 const other = new Person();
 console.log(other.getDescription());
