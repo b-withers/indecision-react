570a8725be5532a93102219458e1c33cb9569ec7
Brady Withers <brady.withers@outlook.com>
2019-02-08 14:45:20 -0700

    nested components

--- src/app.js
+++ src/app.js
@@ -1,3 +1,17 @@
+class IndecisionApp extends React.Component {
+    render () {
+        return (
+    <div>
+        <Header />
+        <Action />
+        <Options />
+        <AddOption />
+    </div>
+        );
+    }
+}
+
+
 class Header extends React.Component {
     render() {
         return (
@@ -24,8 +38,19 @@ class Options extends React.Component {
         return (
             <div>
                 Options Component Here
+                <Option />
             </div>
         );
+    }
+}
+
+class Option extends React.Component {
+    render () {
+        return (
+            <div>
+            Option component Here
+            </div>
+        )
     }
 }
 
@@ -39,13 +64,6 @@ class AddOption extends React.Component {
     }
 }
 
-const jsx = (
-    <div>
-    <Header />
-    <Action />
-    <Options />
-    <AddOption />
-    </div>
-);
 
-ReactDOM.render(jsx, document.getElementById('app'));
+
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
