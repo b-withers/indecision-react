1dca8b9cc06c1f9ebe38a3cb0b73d3bb7ff82018
Brady Withers <brady.withers@outlook.com>
2019-02-08 13:46:10 -0700

    classes example

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,102 +1,38 @@
 'use strict';
 
-console.log('app.js is running!');
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-var app = {
-    title: 'indecision app',
-    subtitle: 'When you can decide what to eat, watch, do, clean, anything... Let me decide!',
-    options: []
-};
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-//adds item from the input to the array
-var onFormSubmit = function onFormSubmit(e) {
-    e.preventDefault();
-    console.log("form submitted");
-    var option = e.target.elements.option.value;
+var Person = function () {
+    function Person() {
+        var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'anonymous';
+        var age = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
 
-    if (option) {
-        //pushes form entry into array
-        app.options.push(option);
-        e.target.elements.option.value = '';
-        renderApp();
+        _classCallCheck(this, Person);
+
+        this.name = name;
+        this.age = age;
     }
-};
 
-//empties array
-var reset = function reset() {
-    app.options = [];
-    renderApp();
-};
+    _createClass(Person, [{
+        key: 'getGreeting',
+        value: function getGreeting() {
+            //return 'Hi I am ' + this.name + '!';
+            return 'Hi. I am ' + this.name + '!';
+        }
+    }, {
+        key: 'getDescription',
+        value: function getDescription() {
+            return this.name + ' is ' + this.age + ' years old.';
+        }
+    }]);
 
-//generates / picks the random item from the array
-var onMakeDecision = function onMakeDecision() {
-    var randomNum = Math.floor(Math.random() * app.options.length);
-    var option = app.options[randomNum];
-    alert(option);
-};
+    return Person;
+}();
 
-var appRoot = document.getElementById('app');
+var me = new Person('Brady Withers', 39);
+console.log(me.getDescription());
 
-var renderApp = function renderApp() {
-    var template = React.createElement(
-        'div',
-        null,
-        React.createElement(
-            'h1',
-            null,
-            app.title
-        ),
-        app.subtitle && React.createElement(
-            'p',
-            null,
-            app.subtitle
-        ),
-        React.createElement(
-            'p',
-            null,
-            app.options.length > 0 ? "Here are your options" : "No options"
-        ),
-        React.createElement(
-            'p',
-            null,
-            React.createElement(
-                'button',
-                { disabled: app.options.length < 2, onClick: onMakeDecision },
-                'Pick for me!'
-            )
-        ),
-        React.createElement(
-            'p',
-            null,
-            React.createElement(
-                'button',
-                { onClick: reset },
-                'remove all'
-            )
-        ),
-        React.createElement(
-            'ol',
-            null,
-            app.options.map(function (items) {
-                return React.createElement(
-                    'li',
-                    { key: items },
-                    items
-                );
-            })
-        ),
-        React.createElement(
-            'form',
-            { onSubmit: onFormSubmit },
-            React.createElement('input', { type: 'text', name: 'option' }),
-            React.createElement(
-                'button',
-                null,
-                'Add Option'
-            )
-        )
-    );
-    ReactDOM.render(template, appRoot);
-};
-
-renderApp();
+var other = new Person();
+console.log(other.getDescription());
